9c192aec2cab618b9bf1a55dd5525a7f6322100a
Graym4c <graham.macisaac09@gmail.com>
2017-06-02 15:30:17 -0700

    made some small modifications

--- tfidfDiffs.py
+++ tfidfDiffs.py
@@ -1,54 +1,47 @@
 import pandas as pd
-from collections import Counter
 import numpy as np
-import nltk
-from nltk.corpus import stopwords
-
 import csv
 import itertools
 import operator
-import numpy as np
-import nltk
 import sys
-from datetime import datetime
-from utils import *
 import re
 
-filepath1 = "C:\Users\Graham\Documents\STA 160\\tfidfparty2017.csv"
-filepath2 = "~\Documents\STA 160\congressional record\congressionalRecords.csv"
+#filepath1 = "C:\Users\Graham\Documents\STA 160\\tfidfparty2017.csv"
+#filepath2 = "~\Documents\STA 160\congressional record\congressionalRecordsSmall.csv"
 
 def tfidfDiffs(filepath1, filepath2):
 
 	"""
-	***WARNING***
-	THIS SCRIPT TAKES ABOUT 10 MINUTES TO RUN PER RECORD RIGHT NOW
-	*************
-
 	Input: filepath1 = tfidf csv
 		   filepath1 = congressional record csv
 	Output: congressional record with mean tfidf difference scores for each text chunk
 			added as a column 
 	"""
 	
+	#LOAD IN TFIDF FILE AND GET WORDS THAT HAVE A NONZERO SIGNIFICANCE
 	dat = pd.read_csv(filepath1)
-
 	dat = dat[dat['idf']>0]
 	dat = dat[dat['party'].isin(['Republican', 'Democrat'])]
 
+	#SPLIT WORDS BY PARTY
 	repwords = dat[dat['party']=='Republican']
 	demwords = dat[dat['party']=='Democrat']
-
 	words = pd.merge(repwords, demwords, on='word', how='outer')
 
+	#DROP UNNESSESARY COLUMNS
 	tfidf = words.drop(['Unnamed: 0_x', 'party_x','n_x', 'tf_x', 'idf_x', 'Unnamed: 0_y', 'party_y','n_y', 'tf_y', 'idf_y'], axis=1)
 	tfidf.columns = ['word', 'tfidf_r', 'tfidf_d']
 
+	#FIND THE DELTA(TFIDF) BETWEEN REPUBLICAN AND DEMOCRAT FOR EVERY WORD
 	diff = [tfidf.loc[x]['tfidf_r'] - tfidf.loc[x]['tfidf_d'] for x in range(len(tfidf))]
 	tfidf['diff'] = diff
 
+	#LOAD THE CONGRESSIONAL FILE
 	data = pd.read_csv(filepath2)
-	text = [re.split(' ', x) for x in data['text']]
-
+	
+	#SPLIT EACH WORD CHUNK SO WE HAVE A LIST OF LISTS. MERGE THE WORDS IN EACH WORD CHUNK
+	#WITH ITS DELTA(TFIDF) SCORE
+	text = [(re.split(' ', x.lower())) for x in data['text']]
 	diffs = []
 	for i in range(len(text)):
 		df = pd.DataFrame(text[i])
